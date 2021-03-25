2b451bce9e0a9c0e253bece27416b707aeea1006
epiczain1234 <mukatizain@gmail.com>
2021-03-25 13:25:49 -0400

    Added collider and floor

---
+++ Assets/Scripts/BirdContoller.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdContoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnMouseDown(){
+        
+    }
+}
